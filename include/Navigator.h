dd2d797aaa5ef34c67d7ca851128186469df1092
m-senol <154716390+m-senol@users.noreply.github.com>
2023-12-27 22:40:31 +0000

    Added 'Maze' class and initiated parser implementation.

--- include/Navigator.h
+++ include/Navigator.h
@@ -2,11 +2,13 @@
 #define __NAVIGATOR_H__
 
 #include "PathFindingStrategy.h"
+#include "Maze.h"
 
 class Navigator
 {
     private:
         PathFindingStrategy *strategy;
+        Maze maze;
 
     public:
         Navigator(PathFindingStrategy strategy);
